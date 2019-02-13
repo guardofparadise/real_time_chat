623fb8be5ffeefff441b8c94691c7e1d24464df4
Vlad <pokidin@ram.com.ua>
2019-02-13 15:59:52 +0200

    ini

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Contact extends Component {
-
-	state = {
-		contacts: [
-			{
-				id: 1,
-				name: 'John Doe',
-				email: 'jdoe@mail.com',
-				phone: '555-555-5555'
-			},
-			{
-				id: 2,
-				name: "Mike", 
-				email: "cool@mail.com" ,
-				phone: "444-444-4444" 
-			},
-			{
-				id: 3,
-				name: "Daniel",
-				email: "cool@mail.com", 
-				phone: "444-444-4444" 
-						
-			}
-		]
-	}
+class Contact extends React.Component {
 
 	render() {
-		const { name, email, phone } = this.props;
+
+		const {name,email,phone} = this.props;
+		
 		return (
 			<div className="card card-body mb-3">
 				<h4>{name}</h4>
